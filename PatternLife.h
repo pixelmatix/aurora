a7c2161be465a2bdf146d6a3451a72487369dcdd
J.B. Langston <jb.langston@gmail.com>
2015-01-09 21:34:20 -0500

    Reduce memory consumption

--- PatternLife.h
+++ PatternLife.h
@@ -29,13 +29,14 @@
 class Cell {
 public:
   byte alive : 1;
-  byte hue: 7;  
+  byte prev : 1;
+  byte hue: 6;  
   byte brightness;
 };
 
 class PatternLife : public Drawable {
 private:
-    Cell world[MATRIX_WIDTH][MATRIX_HEIGHT][2];
+    Cell world[MATRIX_WIDTH][MATRIX_HEIGHT];
     long density = 50;
     int generation = 0;
 
@@ -43,29 +44,28 @@ private:
         for (int i = 0; i < MATRIX_WIDTH; i++) {
             for (int j = 0; j < MATRIX_HEIGHT; j++) {
                 if (random(100) < density) {
-                    world[i][j][0].alive = 1;
+                    world[i][j].alive = 1;
+                    world[i][j].brightness = 255;
                 }
                 else {
-                    world[i][j][0].alive = 0;
+                    world[i][j].alive = 0;
+                    world[i][j].brightness = 0;
                 }
-                world[i][j][0].brightness = 255;
-                world[i][j][0].hue = 0;
-                world[i][j][1].brightness = 255;
-                world[i][j][1].hue = 0;
-                world[i][j][1].alive = 0;
+                world[i][j].prev = world[i][j].alive;
+                world[i][j].hue = 0;
             }
         }
     }
 
     int neighbours(int x, int y) {
-        return (world[(x + 1) % MATRIX_WIDTH][y][0].alive) +
-            (world[x][(y + 1) % MATRIX_HEIGHT][0].alive) +
-            (world[(x + MATRIX_WIDTH - 1) % MATRIX_WIDTH][y][0].alive) +
-            (world[x][(y + MATRIX_HEIGHT - 1) % MATRIX_HEIGHT][0].alive) +
-            (world[(x + 1) % MATRIX_WIDTH][(y + 1) % MATRIX_HEIGHT][0].alive) +
-            (world[(x + MATRIX_WIDTH - 1) % MATRIX_WIDTH][(y + 1) % MATRIX_HEIGHT][0].alive) +
-            (world[(x + MATRIX_WIDTH - 1) % MATRIX_WIDTH][(y + MATRIX_HEIGHT - 1) % MATRIX_HEIGHT][0].alive) +
-            (world[(x + 1) % MATRIX_WIDTH][(y + MATRIX_HEIGHT - 1) % MATRIX_HEIGHT][0].alive);
+        return (world[(x + 1) % MATRIX_WIDTH][y].prev) +
+            (world[x][(y + 1) % MATRIX_HEIGHT].prev) +
+            (world[(x + MATRIX_WIDTH - 1) % MATRIX_WIDTH][y].prev) +
+            (world[x][(y + MATRIX_HEIGHT - 1) % MATRIX_HEIGHT].prev) +
+            (world[(x + 1) % MATRIX_WIDTH][(y + 1) % MATRIX_HEIGHT].prev) +
+            (world[(x + MATRIX_WIDTH - 1) % MATRIX_WIDTH][(y + 1) % MATRIX_HEIGHT].prev) +
+            (world[(x + MATRIX_WIDTH - 1) % MATRIX_WIDTH][(y + MATRIX_HEIGHT - 1) % MATRIX_HEIGHT].prev) +
+            (world[(x + 1) % MATRIX_WIDTH][(y + MATRIX_HEIGHT - 1) % MATRIX_HEIGHT].prev);
     }
 
 public:
@@ -79,7 +79,7 @@ public:
         // Display current generation
         for (int i = 0; i < MATRIX_WIDTH; i++) {
             for (int j = 0; j < MATRIX_HEIGHT; j++) {
-                effects.leds[effects.XY(i, j)] = effects.ColorFromCurrentPalette(world[i][j][0].hue * 2, world[i][j][0].brightness);
+                effects.leds[effects.XY(i, j)] = effects.ColorFromCurrentPalette(world[i][j].hue * 4, world[i][j].brightness);
             }
         }
 
@@ -87,19 +87,17 @@ public:
         for (int x = 0; x < MATRIX_WIDTH; x++) {
             for (int y = 0; y < MATRIX_HEIGHT; y++) {
                 // Default is for cell to stay the same
-                world[x][y][1] = world[x][y][0];
-                if (world[x][y][1].brightness > 0 && world[x][y][1].alive == 0)
-                  world[x][y][1].brightness *= 0.9;
+                if (world[x][y].brightness > 0 && world[x][y].prev == 0)
+                  world[x][y].brightness *= 0.9;
                 int count = neighbours(x, y);
-                if (count == 3 && world[x][y][0].alive == 0) {
+                if (count == 3 && world[x][y].prev == 0) {
                     // A new cell is born
-                    world[x][y][1].alive = 1;
-                    world[x][y][1].hue+=4;
-                    world[x][y][1].brightness = 255;
-                }
-                if ((count < 2 || count > 3) && world[x][y][0].alive == 1) {
+                    world[x][y].alive = 1;
+                    world[x][y].hue += 2;
+                    world[x][y].brightness = 255;
+                } else if ((count < 2 || count > 3) && world[x][y].prev == 1) {
                     // Cell dies
-                    world[x][y][1].alive = 0;
+                    world[x][y].alive = 0;
                 }
             }
         }
@@ -107,9 +105,10 @@ public:
         // Copy next generation into place
         for (int x = 0; x < MATRIX_WIDTH; x++) {
             for (int y = 0; y < MATRIX_HEIGHT; y++) {
-                world[x][y][0] = world[x][y][1];
+                world[x][y].prev = world[x][y].alive;
             }
         }
+
 
         generation++;
         if (generation >= 256)
