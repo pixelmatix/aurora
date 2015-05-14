0a26d63c2376b4fd375e27634fc9c439573ccf45
pup05 <jasoncoon@hotmail.com>
2015-05-13 19:09:58 -0500

    Reformatted ClockDigitalShort.h

--- ClockDigitalShort.h
+++ ClockDigitalShort.h
@@ -30,19 +30,19 @@
 extern tmElements_t time;
 extern bool isTimeAvailable;
 
-enum SetTimeState{
-    SetHour,
-    SetMinute,
+enum SetTimeState {
+  SetHour,
+  SetMinute,
 };
 
 static const unsigned int SetTimeStatesCount = 2;
 SetTimeState SetTimeStates[SetTimeStatesCount] = {
-    SetHour,
-    SetMinute,
+  SetHour,
+  SetMinute,
 };
 
 class ClockDigitalShort : public Drawable {
-public:
+  public:
     char timeBuffer[9];
     uint8_t y = 11;
     rgb24 color = CRGB(CRGB::White);
@@ -50,105 +50,105 @@ public:
     bool twentyFourHour = false;
 
     unsigned int drawFrame() {
-        return drawFrame(y);
+      return drawFrame(y);
     }
 
     unsigned int drawFrame(const int cy) {
-        int x = 1;
+      int x = 1;
 
-        if (isTimeAvailable) {
-            uint8_t hour = time.Hour;
-            if (!twentyFourHour && hour > 12)
-                hour -= 12;
-            else if (!twentyFourHour && hour == 0)
-                hour = 12;
+      if (isTimeAvailable) {
+        uint8_t hour = time.Hour;
+        if (!twentyFourHour && hour > 12)
+          hour -= 12;
+        else if (!twentyFourHour && hour == 0)
+          hour = 12;
 
-            matrix.setForegroundFont(gohufont11b);
-            sprintf(timeBuffer, "%d:%02d", hour, time.Minute);
+        matrix.setForegroundFont(gohufont11b);
+        sprintf(timeBuffer, "%d:%02d", hour, time.Minute);
 
-            if (hour < 10)
-                x = 4;
-        }
-        else {
-            matrix.setForegroundFont(font3x5);
-            sprintf(timeBuffer, "No Clock");
-        }
+        if (hour < 10)
+          x = 4;
+      }
+      else {
+        matrix.setForegroundFont(font3x5);
+        sprintf(timeBuffer, "No Clock");
+      }
 
-        matrix.setScrollOffsetFromTop(MATRIX_HEIGHT);
-        matrix.setScrollColor(color);
-        matrix.clearForeground();
-        matrix.drawForegroundString(x, cy, timeBuffer, true);
+      matrix.setScrollOffsetFromTop(MATRIX_HEIGHT);
+      matrix.setScrollColor(color);
+      matrix.clearForeground();
+      matrix.drawForegroundString(x, cy, timeBuffer, true);
 
-        return 0;
+      return 0;
     }
 
     void drawSetTimeIndicator(SetTimeState state) {
-        int16_t x = 0;
+      int16_t x = 0;
 
-        uint8_t hour = time.Hour;
-        if (!twentyFourHour && hour > 12)
-            hour -= 12;
+      uint8_t hour = time.Hour;
+      if (!twentyFourHour && hour > 12)
+        hour -= 12;
 
-        switch (state) {
-            case SetHour:
-                if (hour > 9)
-                    x = 2;
-                else
-                    x = -1;
-                break;
+      switch (state) {
+        case SetHour:
+          if (hour > 9)
+            x = 2;
+          else
+            x = -1;
+          break;
 
-            case SetMinute:
-                if (hour > 9)
-                    x = 20;
-                else
-                    x = 17;
-                break;
-        }
+        case SetMinute:
+          if (hour > 9)
+            x = 20;
+          else
+            x = 17;
+          break;
+      }
 
-        // upper indicators (in case the clock's at the bottom)
-        if (state != SetHour || hour > 9)
-            matrix.drawTriangle(x + 0, y - 1, x + 1, y - 2, x + 2, y - 1, CRGB(CRGB::SlateGray));
-        matrix.drawTriangle(x + 6, y - 1, x + 7, y - 2, x + 8, y - 1, CRGB(CRGB::SlateGray));
+      // upper indicators (in case the clock's at the bottom)
+      if (state != SetHour || hour > 9)
+        matrix.drawTriangle(x + 0, y - 1, x + 1, y - 2, x + 2, y - 1, CRGB(CRGB::SlateGray));
+      matrix.drawTriangle(x + 6, y - 1, x + 7, y - 2, x + 8, y - 1, CRGB(CRGB::SlateGray));
 
-        // lower indicators (in case the clock's at the top)
-        if (state != SetHour || hour > 9)
-            matrix.drawTriangle(x + 0, y + 11, x + 1, y + 12, x + 2, y + 11, CRGB(CRGB::SlateGray));
-        matrix.drawTriangle(x + 6, y + 11, x + 7, y + 12, x + 8, y + 11, CRGB(CRGB::SlateGray));
+      // lower indicators (in case the clock's at the top)
+      if (state != SetHour || hour > 9)
+        matrix.drawTriangle(x + 0, y + 11, x + 1, y + 12, x + 2, y + 11, CRGB(CRGB::SlateGray));
+      matrix.drawTriangle(x + 6, y + 11, x + 7, y + 12, x + 8, y + 11, CRGB(CRGB::SlateGray));
     }
 
     void drawMoveClockIndicator() {
-        for (int16_t x = 2; x <= 26; x += 6) {
-            // upper indicators (in case the clock's at the bottom)
-            matrix.drawTriangle(x + 0, y - 1, x + 1, y - 2, x + 2, y - 1, CRGB(CRGB::SlateGray));
+      for (int16_t x = 2; x <= 26; x += 6) {
+        // upper indicators (in case the clock's at the bottom)
+        matrix.drawTriangle(x + 0, y - 1, x + 1, y - 2, x + 2, y - 1, CRGB(CRGB::SlateGray));
 
-            // lower indicators (in case the clock's at the top)
-            matrix.drawTriangle(x + 0, y + 11, x + 1, y + 12, x + 2, y + 11, CRGB(CRGB::SlateGray));
-        }
+        // lower indicators (in case the clock's at the top)
+        matrix.drawTriangle(x + 0, y + 11, x + 1, y + 12, x + 2, y + 11, CRGB(CRGB::SlateGray));
+      }
     }
 
     void loadSettings() {
-        y = loadIntSetting("clockY.txt", 2, 11);
-        twentyFourHour = loadIntSetting("clock24h.txt", 2, 11);
-        boundY();
+      y = loadIntSetting("clockY.txt", 2, 11);
+      twentyFourHour = loadIntSetting("clock24h.txt", 2, 11);
+      boundY();
     }
 
     void saveSettings() {
-        saveClockYSetting();
+      saveClockYSetting();
     }
 
     void saveClockYSetting() {
-        saveIntSetting("clockY.txt", y);
+      saveIntSetting("clockY.txt", y);
     }
 
     void saveTwentyFourHourSetting() {
-        saveIntSetting("clock24h.txt", twentyFourHour);
+      saveIntSetting("clock24h.txt", twentyFourHour);
     }
 
     void boundY() {
-        if (y < -1)
-            y = -1;
-        else if (y > 23)
-            y = 23;
+      if (y < -1)
+        y = -1;
+      else if (y > 23)
+        y = 23;
     }
 };
 extern ClockDigitalShort clockDigitalShort;
